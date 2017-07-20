2f4ea48c5be70c733cfde5ee203fd254e80a710b
Vision and Cognition Group <jonathan.r.williford@gmail.com>
2017-07-20 13:16:19 +0200

    Update JCT to use StimSettings__Defaults__.

--- TRACKER_PTB/Curve_Tracing/Experiment/StimSettings/StimSettings_JustCurveTracing.m
+++ TRACKER_PTB/Curve_Tracing/Experiment/StimSettings/StimSettings_JustCurveTracing.m
@@ -6,10 +6,17 @@ global StimObj
 Stm = StimObj.Stm;
 
 %% Load defaults ==========================================================
-eval('StimSettings'); % loads the default parameters
+eval('StimSettings__Defaults__'); % loads the default parameters
+
+Params = StimObj.DefaultParams;
+
+unsaturatedColor = [0.2 0.2 0.2; 0.2 0.2 0.2; .3 .3 .3];
+satLevel = 1/12;
+Params.PawIndCol = satLevel * Params.PawIndCol + (1 - satLevel) * unsaturatedColor;
 
 % use non-blocked curvetracing
 curvetracing = CurveTracingJoystickTask(Params, 'StimSettings/CurveTracingJoyStickTask.csv');
+Stm(1).KeepSubjectBusyTask = curvetracing;
 
 Stm(1).tasksToCycle = [...
     {curvetracing} ... curve tracing
