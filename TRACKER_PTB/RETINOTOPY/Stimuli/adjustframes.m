bb7850ee84e3e5f7ff1a2541bdba927a085149e3
Chris Klink <p.c.klink@gmail.com>
2017-12-15 22:55:42 +0100

    Filesize restrictions fixed for face/walkers

--- TRACKER_PTB/RETINOTOPY/Stimuli/adjustframes.m
+++ TRACKER_PTB/RETINOTOPY/Stimuli/adjustframes.m
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 for rv=1:length(ret_vid)
     for i=1:length(ret_vid(rv).img)
             for rgb=1:3
@@ -6,4 +7,14 @@ for rv=1:length(ret_vid)
                 ret_vid(rv).img{i}(:,:,rgb)=temp_img;
             end
     end
+=======
+for rv=1:length(ret_vid)
+    for i=1:length(ret_vid(rv).img)
+            for rgb=1:3
+                temp_img=ret_vid(rv).img{i}(:,:,rgb);
+                temp_img(temp_img==87)=88;
+                ret_vid(rv).img{i}(:,:,rgb)=temp_img;
+            end
+    end
+>>>>>>> Stashed changes
 end
