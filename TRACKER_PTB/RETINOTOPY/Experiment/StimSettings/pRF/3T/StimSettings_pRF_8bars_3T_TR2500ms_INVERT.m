b54248ab984e0b03ab7adffbc4cf3e2b4e8d49f7
Chris Klink <p.c.klink@gmail.com>
2017-12-17 23:08:56 +0100

    More inverted prf stimuus fixes

--- TRACKER_PTB/RETINOTOPY/Experiment/StimSettings/pRF/3T/StimSettings_pRF_8bars_3T_TR2500ms_INVERT.m
+++ TRACKER_PTB/RETINOTOPY/Experiment/StimSettings/pRF/3T/StimSettings_pRF_8bars_3T_TR2500ms_INVERT.m
@@ -7,7 +7,7 @@ Stm(1).UsePreDefFlipTime=false; %else as fast as possible
 Stm(1).FlipTimePredef = 1/75;
 
 % Background --------------------------------------------------------------
-Stm(1).BackColor = [.5 .5 .5]; % [R G B] 0-1
+Stm(1).BackColor = [.75 .75 .75]; % [R G B] 0-1
 %Stm(1).BackColor = (88/255).*[1 1 1]; % [R G B] 0-1
 % Leuven retinotopy clips have a background of 88 out of 256
 
@@ -19,17 +19,17 @@ Stm(1).FixDotCol = [1 0 0;1 0 0]; %[RGB if not fixating; RGB fixating]
 
 % Fixation position can be toggled with 1-5 keys --------------------------
 Stm(1).Position{1} = [0 0]; % deg from center [vert hor] (-=left/down)
-Stm(1).Position{2} = [-10 -5]; % deg from center (-=left/down)
-Stm(1).Position{3} = [+10 -5]; % deg from center (-=left/down)
-Stm(1).Position{4} = [-10 +5]; % deg from center (-=left/down)
-Stm(1).Position{5} = [+10 +5]; % deg from center (-=left/down)
+Stm(1).Position{2} = [-4 0]; % deg from center (-=left/down)
+Stm(1).Position{3} = [0 -4]; % deg from center (-=left/down)
+Stm(1).Position{4} = [4 0]; % deg from center (-=left/down)
+Stm(1).Position{5} = [0 4]; % deg from center (-=left/down)
 
 % Stimulus position can be cycled automatically every n-th trial
 Stm(1).CyclePosition = 0; % set zero for manual cycling
 
 % Retinotopic mapping stimulus --------------------------------------------
 Stm(1).RandomizeStim=false;
-Stm(1).nRepeatsStimSet=20;
+Stm(1).nRepeatsStimSet=1;
 
 Stm(1).RetMap.StimType{1} = 'ret'; % face / walker / checkerboard
 Stm(1).RetMap.StimType{2} = 'pRF_8bar'; % circle / wedge
@@ -47,7 +47,7 @@ Stm(1).RetMap.StimSize = 16; % [15] degrees (square) >> diameter
 % Maximum size is screen height, will be corrected if it exceeds!
 Stm(1).RetMap.nSteps = 8*20; %(8 directions * number of steps per sweep)
 % (32 for KUL face/walkers; multiple of 8 for 8bar)
-Stm(1).RetMap.nBlanks_after_cardinals = 10; % bars
+Stm(1).RetMap.nBlanks_after_cardinals = 15; % bars
 Stm(1).RetMap.nBlanks_each_nSteps = [0 0]; % wedge/rings
 Stm(1).RetMap.MotionSteps = 15; % number of checker motion steps
 Stm(1).RetMap.fps = 15; % speed of checker motion
@@ -62,7 +62,7 @@ Stm(1).RetMap.chksize = Stm(1).RetMap.BarWidth/4; % bar checker size in deg
 Stm(1).RetMap.LoadFromFile = true;
 % NB! loading a stimulus overwrites all settings with the ones saved!
 Stm(1).RetMap.SaveToFile = false;
-Stm(1).RetMap.FileName = 'pRF_8bars_MOCK_x20.mat';
+Stm(1).RetMap.FileName = 'pRF_8bars_3T_TR2500ms_INVERT.mat';
 
 %% ========================================================================
 % Write stimulus settings to global variable StimObj
--- TRACKER_PTB/RETINOTOPY/Experiment/StimSettings/pRF/MOCK/StimSettings_pRF_8bars_MOCK_TR2500ms_DANNY_1CYC_INVERT.m
+++ TRACKER_PTB/RETINOTOPY/Experiment/StimSettings/pRF/MOCK/StimSettings_pRF_8bars_MOCK_TR2500ms_DANNY_1CYC_INVERT.m
@@ -2,32 +2,32 @@
 global StimObj
 
 %% ========================================================================
-% Refreshrate -------------------------------------------------------------
+%% Refreshrate -------------------------------------------------------------
 Stm(1).UsePreDefFlipTime=false; %else as fast as possible
 Stm(1).FlipTimePredef = 1/75;
 
-% Background --------------------------------------------------------------
-Stm(1).BackColor = [.5 .5 .5]; % [R G B] 0-1
+%% Background --------------------------------------------------------------
+Stm(1).BackColor = 1.5*[.5 .5 .5]; % [R G B] 0-1
 %Stm(1).BackColor = (88/255).*[1 1 1]; % [R G B] 0-1
 % Leuven retinotopy clips have a background of 88 out of 256
 
-% Fixation ----------------------------------------------------------------
-Stm(1).FixDotSize = 0.15;
+%% Fixation ----------------------------------------------------------------
+Stm(1).FixDotSize = 0.1;
 Stm(1).FixDotSurrSize = 0.75;
 Stm(1).FixDotCol = [1 0 0;1 0 0]; %[RGB if not fixating; RGB fixating]
 % fixation color is overwritten by parsettings!
 
-% Fixation position can be toggled with 1-5 keys --------------------------
+%% Stimulus position can be toggled with 1-5 keys --------------------------
 Stm(1).Position{1} = [0 0]; % deg from center [vert hor] (-=left/down)
-Stm(1).Position{2} = [-10 -5]; % deg from center (-=left/down)
-Stm(1).Position{3} = [+10 -5]; % deg from center (-=left/down)
-Stm(1).Position{4} = [-10 +5]; % deg from center (-=left/down)
-Stm(1).Position{5} = [+10 +5]; % deg from center (-=left/down)
+Stm(1).Position{2} = [-4 0]; % deg from center (-=left/down)
+Stm(1).Position{3} = [0 -4]; % deg from center (-=left/down)
+Stm(1).Position{4} = [4 0]; % deg from center (-=left/down)
+Stm(1).Position{5} = [0 4]; % deg from center (-=left/down)
 
 % Stimulus position can be cycled automatically every n-th trial
 Stm(1).CyclePosition = 0; % set zero for manual cycling
 
-% Retinotopic mapping stimulus --------------------------------------------
+%% Retinotopic mapping stimulus --------------------------------------------
 Stm(1).RandomizeStim=false;
 Stm(1).nRepeatsStimSet=1;
 
@@ -40,7 +40,7 @@ Stm(1).RetMap.Dir = 1; % +1 = expanding / ccw, -1 = contracting / cw
 Stm(1).RetMap.TRsPerStep = 1; %s
 Stm(1).RetMap.PreDur_TRs = 5; % volumes
 Stm(1).RetMap.PostDur_TRs = 5; % volumes
-Stm(1).RetMap.nCycles = 1; % 0=unlimited
+Stm(1).RetMap.nCycles = 1; % 0=unlimited << ONLY WORKS FOR CHECKERBOARDS
 
 % This only applies to newly created stim =================================
 Stm(1).RetMap.StimSize = 16; % [15] degrees (square) >> diameter
@@ -62,7 +62,7 @@ Stm(1).RetMap.chksize = Stm(1).RetMap.BarWidth/4; % bar checker size in deg
 Stm(1).RetMap.LoadFromFile = true;
 % NB! loading a stimulus overwrites all settings with the ones saved!
 Stm(1).RetMap.SaveToFile = false;
-Stm(1).RetMap.FileName = 'pRF_8bars_3T_TR2s.mat';
+Stm(1).RetMap.FileName = 'pRF_8bars_MOCK_TR2500ms_DANNY_TRAIN_INVERT.mat';
 
 %% ========================================================================
 % Write stimulus settings to global variable StimObj
